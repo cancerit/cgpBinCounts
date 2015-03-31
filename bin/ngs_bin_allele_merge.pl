5571715648e5d6af45ae94984f5d11bbf6ba7c12
Keiran Raine <keiranmraine@gmail.com>
2015-03-31 13:11:40 +0100

    update licence dates

--- bin/ngs_bin_allele_merge.pl
+++ bin/ngs_bin_allele_merge.pl
@@ -1,9 +1,7 @@
 #!/usr/bin/perl
 
-#./ngs_bin_allele.pl /nfs/cancer_ref01/human/37/genome.cn_bins.csv /nfs/cancer_ref01/human/37/genome.snp6.txt /var/tmp/kr2/ngsbins /nfs/cancer_ref01/nst_links/live/917/PD13371a/PD13371a.bam 21
-
 ########## LICENCE ##########
-# Copyright (c) 2014 Genome Research Ltd.
+# Copyright (c) 2014,2015 Genome Research Ltd.
 #
 # Author: Cancer Genome Project <cgpit@sanger.ac.uk>
 #
@@ -21,6 +19,16 @@
 #
 # You should have received a copy of the GNU Affero General Public License
 # along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+# 1. The usage of a range of years within a copyright statement contained within
+# this distribution should be interpreted as being equivalent to a list of years
+# including the first and last year specified and all consecutive years between
+# them. For example, a copyright statement that reads ‘Copyright (c) 2005, 2007-
+# 2009, 2011-2012’ should be interpreted as being identical to a statement that
+# reads ‘Copyright (c) 2005, 2007, 2008, 2009, 2011, 2012’ and a copyright
+# statement that reads ‘Copyright (c) 2005-2012’ should be interpreted as being
+# identical to a statement that reads ‘Copyright (c) 2005, 2006, 2007, 2008,
+# 2009, 2010, 2011, 2012’."
 ########## LICENCE ##########
 
 use FindBin;
